a1ecf9cfd7697ab73adf8d1c408bb80983cefe10
Vitor Hugo <75076312+Vitor-labs@users.noreply.github.com>
2021-12-20 21:44:39 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,30 +1,71 @@
-#!/usr/bin/env python3
-# Path: app.py
-from datetime import datetime
-from flask import Flask, render_template, url_for, request
+from os import stat
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
 db = SQLAlchemy(app)
 
 
-class tarefa(db.Model):
+class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(20), nullable=False)
-    descricao = db.Column(db.String(100))
-    data = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
-    prioridade = db.Column(db.String(1))
+    name = db.Column(db.String(20), nullable=False)
+    content = db.Column(db.String(80))
     status = db.Column(db.Integer, default=0)
+    prioridade = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<Task %r>' % self.id
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_name = request.form['name']
+        new_task = Task(name=task_name)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Não foi possivel adicionar sua tarefa'
+
+    else:
+        tasks = Task.query.order_by(Task.date_created).all()
+        return render_template('index.html', tasks=tasks)
 
 
-if __name__ == '__main__':
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Task.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'Não foi possivel deletar sua tarefa'
+
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Task.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Não foi possivel alterar sua tarefa'
+
+    else:
+        return render_template('update.html', task=task)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
